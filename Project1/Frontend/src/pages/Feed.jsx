eaf6233d3ee2c893980254a0c2ac23f171e4d358
Charumehra <charumehracm2004@gmail.com>
2026-04-09 14:52:50 +0530

    updated

---
+++ Project1/Frontend/src/pages/Feed.jsx
@@ -0,0 +1,24 @@
+import React, {useState} from 'react'
+
+const Feed = () => {
+
+    const [posts, setPosts] = useState([])
+  return (
+    <section className='feed-section'>
+        {
+            posts.length>0 ? (
+                posts.map((post)=>(
+                    <div key={post._id} className='post-card'>
+                        <img src={post.image} alt={post.caption} />
+                        <p>{post.caption}</p>
+                    </div>
+                ))
+            ) :(
+                <p>No posts available.</p>
+            )
+        }
+    </section>
+  )
+}
+
+export default Feed
