52b72c0150f97c766c018aa618699bae449bb768
Charu Mehra <charmehracm2004@gmail.com>
2026-04-14 14:59:47 +0530

    updated

--- Project1/Frontend/src/pages/Feed.jsx
+++ Project1/Frontend/src/pages/Feed.jsx
@@ -1,28 +1,32 @@
-import React, {useState} from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Feed = () => {
+  const [posts, setPosts] = useState([])
 
-    const [posts, setPosts] = useState([
-        {
-            _id: "1",
-            image: "https://ik.imagekit.io/3c1hykdnu/image_Jai0JS196.jpg",
-            caption: "Sample caption 1"
-        }
-    ])
+  useEffect(() => {
+    axios.get('http://localhost:3000/posts')
+      .then((response) => {
+        setPosts(response.data.post || [])
+      })
+      .catch((error) => {
+        console.error('Failed to load posts', error)
+        setPosts([])
+      })
+  }, [])
+
   return (
-    <section className='feed-section'>
-        {
-            posts.length>0 ? (
-                posts.map((post)=>(
-                    <div key={post._id} className='post-card'>
-                        <img src={post.image} alt={post.caption} />
-                        <p>{post.caption}</p>
-                    </div>
-                ))
-            ) :(
-                <p>No posts available.</p>
-            )
-        }
+    <section className="feed-section">
+      {posts.length > 0 ? (
+        posts.map((post) => (
+          <div key={post._id} className="post-card">
+            <img src={post.image} alt={post.caption} />
+            <p>{post.caption}</p>
+          </div>
+        ))
+      ) : (
+        <p>No posts available.</p>
+      )}
     </section>
   )
 }
