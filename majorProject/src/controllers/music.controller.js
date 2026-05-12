8167b6220bd3b613501efc208378252ff13b2814
Charumehra <174786887+Charumehra@users.noreply.github.com>
2026-05-12 23:56:05 +0530

    updated

--- majorProject/src/controllers/music.controller.js
+++ majorProject/src/controllers/music.controller.js
@@ -1,5 +1,6 @@
 const musicModel = require('../models/music.model');
 const jwt = require('jsonwebtoken');
+const {uploadFile} = require('../services/storage.service')
 
 
 async function createMusic(req, res){
@@ -17,5 +18,28 @@ async function createMusic(req, res){
         }catch(err){
             return res.status(401).json({message: "Unauthorized"});
         }
+
+        const {title} = req.body;
+        const file = req.file;
+
+        const result = await uploadFile(file.buffer.toString('base64'))
+
+        const music = await musicModel.create({
+            uri:result.url,
+            title,
+            artist:decoded.id,
+        })
+
+        res.status(201).json({
+            message: "Music created successfully",
+            music:{
+                id: music._id,
+                uri: music.uri,
+                title: music.title,
+                artist: music.artist,
+            }
+        })
     
 }
+
+module.exports = {createMusic}
--- majorProject/src/services/storage.service.js
+++ majorProject/src/services/storage.service.js
@@ -2,7 +2,16 @@ const {ImageKit} = require('@imagekit/nodejs')
 
 const imageKitClient = new ImageKit({
     privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
-    publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
-    urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT
 })
 
+async function uploadFile(file){
+    const result = await imageKitClient.files.upload({
+        file,
+        fileName:"music_" + Date.now,
+        folder:"complete_backend" + Date.now
+    })
+
+    return result
+}
+
+module.exports={uploadFile}
